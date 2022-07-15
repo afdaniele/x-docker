b3fa2608c8a0a6b3766e8079546a50ef35d3172e
Andrea F. Daniele <afdaniele@ttic.edu>
2022-07-15 18:10:56 -0500

    python/macOS: bug fix

--- python/include/xdocker/lib_macos.py
+++ python/include/xdocker/lib_macos.py
@@ -18,7 +18,7 @@ class XDockerMacOSConfigurator(XDockerConfigurator):
             ["defaults", "write", "org.macosforge.xquartz.X11", "enable_iglx", "-bool", "true"])
         # start XQuartz
         app_count = subprocess.check_output(
-            "ps aux | grep XQuartz | grep -v grep | wc -l | awk '{print $1}'", shell=True).decode("utf-8").strip()
+            "ps aux | grep XQuartz | grep -v grep | wc -l | awk '{print $1}'", shell=True).decode("utf-8").split('\n')[0]
         try:
             app_count = int(app_count)
         except ValueError:
@@ -30,15 +30,15 @@ class XDockerMacOSConfigurator(XDockerConfigurator):
             # time.sleep(4)
         # get default gateway device
         NET_DEVICE = subprocess.check_output(
-            "route get default | grep 'interface: ' | head -1 | awk '{ print $2 }'", shell=True).decode("utf-8").strip()
+            "route get default | grep 'interface: ' | head -1 | awk '{ print $2 }'", shell=True).decode("utf-8").split('\n')[0]
         print(f"X-Docker: Detected default gateway is: {NET_DEVICE}")
         # get ip
         NET_IP = subprocess.check_output(
-            "ifconfig %s | grep inet | awk '$1==\"inet\" {print $2}'" % NET_DEVICE, shell=True).decode("utf-8").strip()
+            "ifconfig %s | grep inet | awk '$1==\"inet\" {print $2}'" % NET_DEVICE, shell=True).decode("utf-8").split('\n')[0]
         print(f"X-Docker: Detected gateway device IP is: {NET_IP}")
         # get display number
         DISPLAY_NO = subprocess.check_output(
-            "ps -ef | grep \"Xquartz :\" | grep -v xinit | awk '{ print $9; }'", shell=True).decode("utf-8").strip()
+            "ps -ef | grep \"Xquartz :\" | grep -v xinit | awk '{ print $9; }'", shell=True).decode("utf-8").split('\n')[0]
         print(f"X-Docker: Detected display number is: {DISPLAY_NO}")
         environment["DISPLAY"] = f"{NET_IP}{DISPLAY_NO}"
         # allow X-server to receive connections from current machine

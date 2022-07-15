f72e37f2dafac315ae63c7e3ae96894feb61cb49
Andrea F. Daniele <afdaniele@ttic.edu>
2022-07-15 17:35:31 -0500

    python/macOS: bug fix

--- python/include/xdocker/lib_macos.py
+++ python/include/xdocker/lib_macos.py
@@ -24,21 +24,25 @@ class XDockerMacOSConfigurator(XDockerConfigurator):
         except ValueError:
             app_count = 0
         if app_count <= 0:
-            # start Xquartz
-            subprocess.check_call("open -a XQuartz", shell=True)
-            time.sleep(4)
+            print("WARNING: You need to launch XQuartz")
+            # # start Xquartz
+            # subprocess.check_call("open -a XQuartz", shell=True)
+            # time.sleep(4)
         # get default gateway device
         NET_DEVICE = subprocess.check_output(
             "route get default | grep 'interface: ' | head -1 | awk '{ print $2 }'", shell=True).decode("utf-8").strip()
+        print(f"X-Docker: Detected default gateway is: {NET_DEVICE}")
         # get ip
         NET_IP = subprocess.check_output(
             "ifconfig %s | grep inet | awk '$1==\"inet\" {print $2}'" % NET_DEVICE, shell=True).decode("utf-8").strip()
+        print(f"X-Docker: Detected gateway device IP is: {NET_IP}")
         # get display number
         DISPLAY_NO = subprocess.check_output(
-            "ps -ef | grep \"Xquartz :\" | grep -v xinit | awk '{ print $9; }'", shell=True).decode("utf-8").strip()
+            "ps -ef | grep \"Xquartz :\" | grep -v xinit | awk '{ print $9; }'", shell=True).decode("utf-8").strip(f)
+        print(f"X-Docker: Detected display number is: {NET_DEVICE}")
         environment["DISPLAY"] = DISPLAY_NO
         # allow X-server to receive connections from current machine
-        subprocess.check_call(f"DISPLAY={DISPLAY_NO} xhost + {NET_IP}", shell=True)
+        subprocess.check_call(f"xhost + {NET_IP}", shell=True, env={"DISPLAY": DISPLAY_NO})
         # ---
         config["environment"] = environment
         return config

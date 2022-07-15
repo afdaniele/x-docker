6f77c65d34c9b87394d655a80d1e745aa747264d
Andrea F. Daniele <afdaniele@ttic.edu>
2022-07-15 17:08:51 -0500

    implementing MacOS version of python library

--- python/include/xdocker/lib_macos.py
+++ python/include/xdocker/lib_macos.py
@@ -1,3 +1,6 @@
+import subprocess
+import time
+
 from docker import DockerClient
 
 from xdocker.lib import XDockerConfigurator
@@ -7,9 +10,38 @@ class XDockerMacOSConfigurator(XDockerConfigurator):
 
     @staticmethod
     def get_configuration(client: DockerClient) -> dict:
-        raise NotImplementedError("X-Docker does not support MacOS at this time. "
-                                  "You can upvote the corresponding issue on "
-                                  "'https://github.com/afdaniele/x-docker/issues/1'.")
+        # prepare output
+        environment = {}
+        config = {}
+        # give XQuartz permissions to run iglx
+        subprocess.check_call(
+            ["defaults", "write", "org.macosforge.xquartz.X11", "enable_iglx", "-bool", "true"])
+        # start XQuartz
+        app_count = subprocess.check_output(
+            "ps aux | grep $APP_NAME | grep -v grep | wc -l | awk '{print $1}'", shell=True)
+        try:
+            app_count = int(app_count)
+        except ValueError:
+            app_count = 0
+        if app_count <= 0:
+            # start Xquartz
+            subprocess.check_call("open -a XQuartz")
+            time.sleep(4)
+        # get default gateway device
+        NET_DEVICE = subprocess.check_output(
+            "route get default | grep 'interface: ' | head -1 | awk '{ print $2 }'")
+        # get ip
+        NET_IP = subprocess.check_output(
+            "ifconfig %s | grep inet | awk '$1==\"inet\" {print $2}'" % NET_DEVICE)
+        # get display number
+        DISPLAY_NO = subprocess.check_output(
+            "ps - ef | grep \"Xquartz :\" | grep -v xinit | awk '{ print $9; }'")
+        environment["DISPLAY"] = DISPLAY_NO
+        # allow X-server to receive connections from current machine
+        subprocess.check_call(f"xhost + {NET_IP}")
+        # ---
+        config["environment"] = environment
+        return config
 
 
 __all__ = [
